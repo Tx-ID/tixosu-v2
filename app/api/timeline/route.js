a920916fce8cb498bfe737d3971be16863f96742
istamarahsan <istamarahsan@gmail.com>
2023-10-23 09:09:27 +0700

    timeline route create

--- app/api/timeline/route.js
+++ app/api/timeline/route.js
@@ -1,6 +1,9 @@
 import * as Turso from "@/lib/turso"
 import * as Timeline from "@/lib/timeline/timeline"
 import { NextResponse } from "next/server";
+import { DateTime } from "luxon";
+import { getServerSession } from "next-auth";
+import * as auth from "@/lib/auth";
 
 export async function GET(req) {
     const tursoClient = Turso.create()
@@ -15,4 +18,30 @@ export async function GET(req) {
             end: e.end.toISO()
         }))
     })
+}
+
+/**
+ * 
+ * @param {Request} req 
+ */
+export async function POST(req) {
+
+    const session = await getServerSession(auth.config)
+
+    if (!(session?.user?.is_admin)) {
+        return new NextResponse('"Unauthorized"', {
+            status: 401
+        });
+    }
+
+    const tursoClient = Turso.create()
+    const body = await req.json()
+
+    const created = await Timeline.addTimelineEvent(tursoClient, body.id, {
+        name: body.name,
+        start: DateTime.fromISO(body.start),
+        end: DateTime.fromISO(body.end)
+    })
+
+    return NextResponse.json(created)
 }
