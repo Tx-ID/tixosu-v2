3cf0ca4a25c8169b1a2e0554c7b6986bf9d585b1
istamarahsan <istamarahsan@gmail.com>
2023-12-17 12:09:02 +0700

    test opengraph

--- app/(guest)/layout.js
+++ app/(guest)/layout.js
@@ -1,14 +1,31 @@
-import './globals.css'
+import "./globals.css";
 
-import { Lexend } from 'next/font/google'
-const lexend = Lexend({ subsets: ['latin'] })
+import { Lexend } from "next/font/google";
+const lexend = Lexend({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="dark">
-      <body className={"min-h-screen max-w-screen-md m-auto text-default-white " + lexend.className}>
+      <head>
+        <title>Osu Tourney Demo</title>
+        <meta property="description" content="" />
+        <meta
+          property="og:image"
+          content="https://cdn.gilcdn.com/ContentMediaGenericFiles/82a932127b036953af37baa6a312bf79-Full.webp"
+        />
+        <meta
+          property="og:description"
+          content="Community tournaments without the hassle"
+        />
+      </head>
+      <body
+        className={
+          "min-h-screen max-w-screen-md m-auto text-default-white " +
+          lexend.className
+        }
+      >
         {children}
       </body>
     </html>
-  )
+  );
 }
